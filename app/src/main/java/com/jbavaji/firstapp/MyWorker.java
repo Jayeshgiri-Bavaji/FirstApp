ae7f2728f131c8c0cc1b3e7101aca629e8060900
Jayeshgiri.Bavaji <jayeshgiri.bavaji@synechron.com>
2019-03-27 15:40:26 +0530

    First App Sample From Plural Sight

---
+++ app/src/main/java/com/jbavaji/firstapp/MyWorker.java
@@ -0,0 +1,13 @@
+package com.jbavaji.firstapp;
+
+/**
+ * Created by Jayeshgiri Bavaji on 3/27/2019.
+ * Synechron Technologies
+ * jayeshgiri.bavaji@synechron.com
+ */
+public class MyWorker {
+
+    public static int doubleTheValue(int value){
+        return value * 2;
+    }
+}
